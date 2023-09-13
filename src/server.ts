fe5db415f9b662264181d3510a42f23ce3b9e1f5
Felipe Antunes <felipeantunes@192.168.1.3>
2023-09-13 20:43:23 -0300

    get movies endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
-
 import { Router, Request, Response } from "express";
+import cheerio from "cheerio";
+import qs from "qs";
 
 const app = express();
 
@@ -8,8 +9,34 @@ const route = Router();
 
 app.use(express.json());
 
+function getMoviesSearch(body: string) {
+  const $ = cheerio.load(body);
+  let movies: any = [];
+  $("ul.ipc-metadata-list > li").each(function (this: cheerio.Element) {
+    const link = $(this).find("a");
+    const movie = {
+      name: link.text(),
+      id: link.attr("href")?.split("/")[2],
+    };
+    movies.push(movie);
+  });
+
+  return movies;
+}
+
 route.get("/", (req: Request, res: Response) => {
-  res.json({ message: "hello world with Typescript" });
+  res.json({ messae: "Hello world" });
+});
+
+route.get("/movies", async (req: Request, res: Response) => {
+  const url = `https://www.imdb.com/find/?${qs.stringify(req.query)}`;
+  const response = await fetch(url);
+
+  const body = await response.text();
+
+  const movies = await getMoviesSearch(body);
+
+  res.json(movies);
 });
 
 app.use(route);
